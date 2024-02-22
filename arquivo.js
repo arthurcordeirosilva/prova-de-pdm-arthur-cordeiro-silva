56b2746da217b5f7891aead0536df6fe679ce4da
arthur_mbb <145819446+arthurcordeirosilva@users.noreply.github.com>
2024-02-22 14:59:14 -0300

    Update arquivo.js

--- arquivo.js
+++ arquivo.js
@@ -1,4 +1,4 @@
-const aluno1 = {
+let aluno1 = {
     nome: "Jorge Miguel",
     idade: 15,
     cursando: ["PDM, BD, TIA"],
@@ -13,30 +13,30 @@ const aluno2 = {
     ra: [6236],
 }
 //esta função valida o cadastro dos alunos
-function validarCadastro(aluno1) { 
+function validarCadastro(aluno) { 
 
-if (aluno1.nome >= 10) {
+if (aluno.nome.lenght >= 10) {
 
    return console.log("nome bonito")
 } else{ 
     return console.log("nome pequeno demais")
 }
-if (aluno1.idade > 14) {
+if (aluno.idade > 14) {
    return console.log("idade certa")
 } else {
  return console.log("não tem a idade minima")
 }
-if (aluno1.cursando >= 3) {
+if (aluno.cursando.lenght >= 3) {
     return console.log("cursos ok")
 } else {
     return console.log("cursa poucas materias")
 }
-if (aluno1.presença >= 2) {
+if (aluno.presença >= 2) {
     return console.log("vai bastante a aula")
 } else{
     return console.log("falta muito")
 }
-if (aluno1.ra = 7){
+if (aluno.ra === 7){
 
     return console.log("ra correto")
 } else {
@@ -45,36 +45,3 @@ if (aluno1.ra = 7){
 
 }
 validarCadastro(aluno1)
-
-function validarCadastro(aluno2) { 
-
-    if (aluno2.nome >= 10) {
-    
-       return console.log("nome bonito")
-    } else{ 
-        return console.log("nome pequeno demais")
-    }
-    if (aluno2.idade > 14) {
-       return console.log("idade certa")
-    } else {
-     return console.log("não tem a idade minima")
-    }
-    if (aluno2.cursando >= 3) {
-        return console.log("cursos ok")
-    } else {
-        return console.log("cursa poucas materias")
-    }
-    if (aluno2.presença >= 2) {
-        return console.log("vai bastante a aula")
-    } else{
-        return console.log("falta muito")
-    }
-    if (aluno2.ra = 7){
-    
-        return console.log("ra correto")
-    } else {
-        return console.log("ra errado")
-    }
-    
-    }
-    validarCadastro(aluno2)
